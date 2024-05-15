f43adeaa5ca933f0382336730216fd92afe00f0e
NahinNilav <ehsannilav@gmail.com>
2024-05-15 13:26:12 +0600

    Added Cuisine and Customer class. Work left on Database class

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -10,6 +10,7 @@ module org.example.restaurant_management_system {
     requires org.kordamp.bootstrapfx.core;
     requires eu.hansolo.tilesfx;
     requires com.almasb.fxgl.all;
+    requires java.sql;
 
     opens org.example.restaurant_management_system to javafx.fxml;
     exports org.example.restaurant_management_system;
